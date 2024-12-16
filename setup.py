01559de0de69238eca22e384d8cbd653f35142eb
Paris Qian <1759658+qiansen1386@users.noreply.github.com>
2024-12-17 02:30:55 +0800

    fix: 解决循环依赖问题，在构建时移除对requests的依赖

--- setup.py
+++ setup.py
@@ -1,38 +1,46 @@
-'''
+"""
 理杏仁开放平台API包安装
-'''
+"""
 
-import lixinger_openapi as lo
+import os
 from setuptools import find_packages, setup
 
+
 # 获取版本号
-version = lo.__version__
+def get_version():
+    version_file = os.path.join(
+        os.path.dirname(__file__), "lixinger_openapi", "_version.py"
+    )
+    with open(version_file, "r", encoding="utf-8") as f:
+        exec(f.read())
+    return locals()["__version__"]
+
 
 # 读取README.md内容作为长描述
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='lixinger_openapi',
-    version=version,
-    description='lixinger openapi',
+    name="lixinger_openapi",
+    version=get_version(),
+    description="lixinger openapi",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    author='sheki lyu',
-    author_email='lvxueji@gmail.com',
-    license='Apache License v2',
+    author="sheki lyu",
+    author_email="lvxueji@gmail.com",
+    license="Apache License v2",
     install_requires=[
         "requests>=1.0.0",
         "pandas>=1.0.0",
     ],
-    url='https://github.com/ShekiLyu/lixinger-openapi',
+    url="https://github.com/ShekiLyu/lixinger-openapi",
     classifiers=[
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',  # 移除对Python 2.7的支持
-        'License :: OSI Approved :: Apache Software License',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",  # 移除对Python 2.7的支持
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    python_requires='>=3.6',  # 指定最低Python版本要求
+    python_requires=">=3.6",  # 指定最低Python版本要求
 )
